1974ebcf0673c84e6181b8d4c589dce76618b71c
Andrew Rich <andrewr@navigatingcancer.com>
2018-03-14 17:31:55 -0700

    boilerplate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocaleappTest::Application.config.secret_key_base = '5a00ff7adef6134ddc68d67ed1c304693b103be522cc155181ce51ff1167d61101dc4480e75901c5404b632d937b77c15853463f33b2822cc9b1180ad4f3081d'
